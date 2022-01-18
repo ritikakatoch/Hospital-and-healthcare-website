8bdfd17b6b5a5bbbea675a653065e3304c16976f
Ritika Katoch <71933308+Ritikakatoch0128@users.noreply.github.com>
2022-01-18 21:51:03 +0530

    files moved to other dir

--- Home/toggle.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-		$("#info").click(function(){
-			$("#hideshow").toggle();
-	});
-});
--- Home/warning.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$("#close").click(function(){
-		//$(".notification").hide();
-		$(".navbar .fixed-top").css("top","0px");
-	});
-});
